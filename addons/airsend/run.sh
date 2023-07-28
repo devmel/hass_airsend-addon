1cc2efecc8cc01db86c2d4997611767b2056d309
Devmel Apps <apps@devmel.com>
2023-07-28 13:47:59 +0200

    internal_url

--- addons/airsend/run.sh
+++ addons/airsend/run.sh
@@ -3,14 +3,21 @@
 arch="$(apk --print-arch)"
 case "$arch" in \
 		aarch64) arch='arm64' ;; \
+		armhf) arch='armhf' ;; \
 		armv7) arch='arm' ;; \
 		amd64) arch='x86_64' ;; \
 		i386) arch='x86' ;; \
 	esac;
-echo "AirSendWebService arch ${arch}"
+echo "AirSendWebService arch: ${arch}"
+hname="$(hostname -i)"
+echo "internal_url: http://${hname}:33863"
+
+if [ -n "${SUPERVISOR_TOKEN:-}" ]; then
+	echo ${SUPERVISOR_TOKEN} > hass_api.token
+fi
 
 cd /home
-echo ${SUPERVISOR_TOKEN} > hass_api.token
+ulimit -n 4096
 ./bin/unix/${arch}/AirSendWebService 99399
 php -S 127.0.0.1:80 callback.php
 sleep infinity
